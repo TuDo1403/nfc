d80d1b4c27a3c4cbb2a85d827bd579cc2a8414f4
TuDo1403 <tudo.dev@gmail.com>
2022-09-19 21:17:48 +0700

    chore: add erc20 test

---
+++ contracts/tests/ERC20Test.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "../external/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract ERC20Test is ERC20Permit {
+    constructor(string memory name_, string memory symbol_)
+        payable
+        ERC20(name_, symbol_, 18)
+        ERC20Permit(name_)
+    {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
